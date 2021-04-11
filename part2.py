bba99ba3c8b3d8b4bae8e4fec61f0ea83a463d57
Justin Pyun <>
2021-04-11 18:58:31 +0000

    I copied code asking for text which could change a numerical input into a recorded numerical value before adding them up at the code in line 27

--- part2.py
+++ part2.py
@@ -2,14 +2,12 @@
 ______
 PART 2
 ______
-
 Run the code below. Fix it so that the program will print the product of the number 
 and 10 in the formatted sentence that's already there, and the sum of 99 and the 
 number in the formatted sentence that's already there.
-
 '''
 
 #code starts here
-number = input("Enter a number: ")
+number = int(input("Enter a number: "))
 print("Ten times your number is", 10 * number)
 print("Ninety-nine more than your number is", 99 + number)
